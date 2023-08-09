00e258ecf7a08e01218143574e91cf61c01b5139
Sebastian <sebastian.lescano@neoris.com>
2023-08-09 17:43:56 -0300

    End section 2

---
+++ ReStore/API/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using API.Entities;
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        { 
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ReStore/API/Program.cs
@@ -0,0 +1,42 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+var scope= app.Services.CreateScope();//obtengo el scope
+var context = scope.ServiceProvider.GetRequiredService<StoreContext>();//obtengo el contexto
+var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();//obtengo el logger
+try
+{
+    context.Database.Migrate();//migrar la base de datos
+    DbInitializer.Initialize(context);//inicializar la base de datos
+}
+catch (Exception ex)
+{
+    logger.LogError(ex, "An error occurred during migration");
+}
+app.Run();
